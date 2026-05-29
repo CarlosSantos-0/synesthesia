f83d6f6f0642ddbe5369df85ece2aa4307dfef48
CarlosSantos-0 <carlos.silva3@aluno.ifsp.edu.br>
2026-05-29 20:59:08 -0300

    Criando o Palco

---
+++ src/pages/Stage/Stage.jsx
@@ -0,0 +1,14 @@
+import styles from '../Stage/Stage.modules.css'
+
+function Stage() {
+
+    return (
+        <>
+            <div className={styles.stage}>
+                <h1> Esse é o Palco </h1>
+            </div>
+        </>
+    )
+}
+
+export default Stage
